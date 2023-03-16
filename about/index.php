780fb4a40d78406a262d6ec46cb6c80631c9b9f9
Tristan White <oliver.tristan@gmail.com>
2023-03-16 14:09:04 +0000

    Adjust font-size

--- about/index.php
+++ about/index.php
@@ -1,4 +1,4 @@
-<?php
+ class="large"<?php
 include("../_inc/_app.php");
 $cd = new ColorDrop;
 include("../_inc/_functions.php");
@@ -13,11 +13,11 @@ include("../_inc/header.php");
 		<div class="row center-tiny">
 			<div class="tiny-12 small-10 medium-8 large-6">
 				<h1>About</h1>
-				<p>ColorDrop is an online tool that allows users to create, save, and share color palettes. The website offers a simple, user-friendly interface that allows you to easily create new color palettes, or browse through existing ones.</p>
-				<p>One of the unique features of ColorDrop is its ability to extract color palettes from images. Users can upload an image to the website, and ColorDrop will analyze the colors in the image to generate a new color palette based on those colors. This can be a great way to generate color schemes that are visually harmonious and match the overall feel of a particular image.</p>
-				<p>Users can also save their color palettes by "liking" them, and share them with others through a unique URL. This makes it easy to collaborate on color schemes with team members or clients.</p>
-				<p>In addition to creating and sharing color palettes, ColorDrop also offers a variety of other tools and resources related to color, such as color theory guides and tutorials on how to use color in design.</p>
-				<p>Overall, ColorDrop is a useful tool for anyone who works with color, whether you're a designer, artist, or just someone who wants to experiment with different color combinations.</p>
+				<p class="large">ColorDrop is an online tool that allows users to create, save, and share color palettes. The website offers a simple, user-friendly interface that allows you to easily create new color palettes, or browse through existing ones.</p>
+				<p class="large">One of the unique features of ColorDrop is its ability to extract color palettes from images. Users can upload an image to the website, and ColorDrop will analyze the colors in the image to generate a new color palette based on those colors. This can be a great way to generate color schemes that are visually harmonious and match the overall feel of a particular image.</p>
+				<p class="large">Users can also save their color palettes by "liking" them, and share them with others through a unique URL. This makes it easy to collaborate on color schemes with team members or clients.</p>
+				<p class="large">In addition to creating and sharing color palettes, ColorDrop also offers a variety of other tools and resources related to color, such as color theory guides and tutorials on how to use color in design.</p>
+				<p class="large">Overall, ColorDrop is a useful tool for anyone who works with color, whether you're a designer, artist, or just someone who wants to experiment with different color combinations.</p>
 			</div>
 		</div>
 	</div>
