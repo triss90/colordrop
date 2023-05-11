e9405618831773c9633671eb8ec518641dd83890
Tristan White <oliver.tristan@gmail.com>
2023-05-11 08:16:16 +0200

    Add cookie consent form

--- _inc/footer.php
+++ _inc/footer.php
@@ -4,6 +4,8 @@
 		Powered by <a href="https://cloudservers.dk" target="_blank">cloudservers.dk</a>
 		<br>
 		Available on <a href="https://github.com/triss90/colordrop" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-github" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" style="position:relative;top:2px;"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path></svg>GitHub</a>
+		<br><br>
+		<a href="#" id="open_preferences_center">Update cookies preferences</a>
 	</p>
 </footer>
 <div class="totop" id="totop" onclick="scrollToTop();">^</div>
@@ -11,5 +13,25 @@
 	<div class="close-button" onclick="carbonAdToggle(this)">&#10005;</div>
 	<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CKYIP2QL&placement=colordropio" id="_carbonads_js"></script>
 </div>
+
+<!-- Accept Cookies -->
+<script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js" charset="UTF-8"></script>
+<script type="text/javascript" charset="UTF-8">
+document.addEventListener('DOMContentLoaded', function () {
+cookieconsent.run({"notice_banner_type":"simple","consent_type":"express","palette":"light","language":"en","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"ColorDrop"});
+});
+</script>
+
+<!-- Google Analytics -->
+<script type="text/plain" data-cookie-consent="tracking" async src="https://www.googletagmanager.com/gtag/js?id=G-9S16HNQPFS"></script>
+<script type="text/plain" data-cookie-consent="tracking">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9S16HNQPFS');
+</script>
+<!-- end of Google Analytics-->
+
 </body>
 </html>
