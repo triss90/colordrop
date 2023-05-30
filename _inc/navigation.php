a02a588cc16def5d5d282ac40bc64d29fcef17a1
Tristan White <oliver.tristan@gmail.com>
2023-05-30 15:06:32 +0200

    Update navigation to reflect prod

--- _inc/navigation.php
+++ _inc/navigation.php
@@ -9,64 +9,10 @@
 		</svg>
 	</button>
 	<nav id="navigation">
-		<a class="tools" href="/tools">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			   <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
-			   <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-			</svg> -->
-			Tools
-		</a>
-		<a class="palettes" href="/">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-palette" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			   <path d="M12 21a9 9 0 0 1 0 -18c4.97 0 9 3.582 9 8c0 1.06 -.474 2.078 -1.318 2.828c-.844 .75 -1.989 1.172 -3.182 1.172h-2.5a2 2 0 0 0 -1 3.75a1.3 1.3 0 0 1 -1 2.25"></path>
-			   <path d="M8.5 10.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
-			   <path d="M12.5 7.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
-			   <path d="M16.5 10.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
-			</svg> -->
-			Palettes
-		</a>
-		<a class="flat" href="/flat">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-paint" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			   <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"></path>
-			   <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2"></path>
-			   <path d="M10 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
-			</svg> -->
-			Flat Colors
-		</a>
-		<a class="gradients" href="/gradients">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-gradienter" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			   <path d="M3.227 14c.917 4 4.497 7 8.773 7c4.277 0 7.858 -3 8.773 -7"></path>
-			   <path d="M20.78 10a9 9 0 0 0 -8.78 -7a8.985 8.985 0 0 0 -8.782 7"></path>
-			   <path d="M12 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-			</svg> -->
-			Gradients
-		</a>
-		<a class="scan" href="/scan">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-scan-eye" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-				<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-				<path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
-				<path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
-				<path d="M16 4h2a2 2 0 0 1 2 2v2"></path>
-				<path d="M16 20h2a2 2 0 0 0 2 -2v-2"></path>
-				<path d="M7 12c3.333 -4.667 6.667 -4.667 10 0"></path>
-				<path d="M7 12c3.333 4.667 6.667 4.667 10 0"></path>
-				<path d="M12 12h-.01"></path>
-			</svg> -->
-			Scan Image
-		</a>
-		<a class="about" href="/about">
-			<!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-question" width="1rem" height="1rem" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-			   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			   <path d="M6 21v-2a4 4 0 0 1 4 -4h3.5"></path>
-			   <path d="M19 22v.01"></path>
-			   <path d="M19 19a2.003 2.003 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
-			   <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-			</svg> -->
-			About
-		</a>
+		<a class="palettes" href="/">Palettes</a>
+		<a class="flat" href="/flat">Flat Colors</a>
+		<a class="gradients" href="/gradients">Gradients</a>
+		<a class="scan" href="/scan">Scan Image</a>
+		<a class="about" href="/about">About</a>
 	</nav>
 </div>
